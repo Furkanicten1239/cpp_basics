1baec483af27a7083b7ba4e5cfdc73c0e623b1cb
Furkan İçten <furkanicten1239@gmail.com>
2025-10-13 19:49:15 +0300

    practice 1 example 3 has been solved and prepared as a notebook.

--- practices_1.cpp
+++ practices_1.cpp
@@ -2,8 +2,19 @@
 #include <cmath>
 using namespace std;
 
+
+
+//                          FIRST QUESTION  
+// Now write the function with the prototype bool isPrime(int n); to check if the input n is prime.
 // firstly, I made it suitable for given input in terminal
-int main(){
+/*
+Consider the following approach to check if a number is prime:
+• A prime number has no divisors except 1 and itself.
+• It is enough to check divisors only up to √n.
+• Use the cmath library and the sqrt() function.
+*/
+
+int is_prime(){
     int number;
     cin >> number;
     cout << "sectigin sayi: " << number << endl;
@@ -38,6 +49,76 @@ bool isPrime(int n){
     }
     if (isprime == true) return true;
     else  return false;
+}
 
+
+
+
+
+//              SECOND QUESTION
+
+/*
+Write a function with the prototype bool isSquare(int n); to check if an integer is a perfectsquare.
+• Use sqrt() from cmath.
+• Return true if the number is a square, otherwise false.
+*/
+
+
+bool isSquare(int n){
+
+    int int_sqrt;
+    int_sqrt = sqrt(n);
+
+    // We used that print methods to check the output.
+    //cout << sqrt(n) << endl;
+    //cout << int_sqrt << endl;
+    if (n < 0) return false;
+    return (sqrt(n) == int_sqrt);
+
+}
+
+// Or you can directly check their multiplication is equal to the integer n or not.
+
+bool isSquare2(int n){
+    int sqrt_n = sqrt(n);
+    if (n < 0) return false;
+    return (sqrt_n * sqrt(n) == n);
+}
+
+
+
+
+
+
+
+//              THIRD QUESTION
+
+/*
+Write a function
+    void checkWithFunction(int n, bool (*func)(int))that:
+
+    • Receives an integer and a function pointer to a function of type bool(int).
+
+    • Calls the function on the integer and writes
+        o "The result is true" if the function returns true.
+        o "The result is false" otherwise.
+
+*/
+
+
+void checkWithFunction(int n, bool (*func) (int)){
+
+    std::cout << func(n) << endl;
+
+}
+
+int main(){
+    int n = -2;
+    std::cout << "is number square ? " << isSquare(n) << endl;
+    std::cout << "is number square ? " << isSquare2(n) << endl;
+
+    checkWithFunction(10, &isSquare);
+    
+    return 0;
 }
 
